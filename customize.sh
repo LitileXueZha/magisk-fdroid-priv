73c7a3f507a4bcfe63319f55ed9db524a99a4675
诸葛林 <ktnig@qq.com>
2024-02-21 12:39:52 +0800

    fix

--- customize.sh
+++ customize.sh
@@ -1,23 +1,26 @@
-#!/sbin/sh
+#!/system/bin/sh
 
 # https://topjohnwu.github.io/Magisk/guides.html#customization
 #
-MODDIR=${0%/*}
 
-APKDIR="$MODDIR/system/priv-app/FDroidPrivilegedExtension"
+
+APK="$MODPATH/system/priv-app/FDroidPrivilegedExtension/base.apk"
 SHA256="1008525a17b4f6a93ac690f9c50dcb675b6bebf53d2879dbc98ba65a1cb2e28d"
 APPURL="https://f-droid.org/repo/org.fdroid.fdroid.privileged_2130.apk"
 
-if [ ! -f $APKDIR/base.apk ];then
-    wget -P $APKDIR -O base.apk $APPURL
+if [ ! -f $APK ];then
+    ui_print "downloading apk..."
+    wget --no-check-certificate -T 30 -O $APK $APPURL
 fi
-ret=`sha256sum $APKDIR/base.apk`
+ret=`sha256sum $APK | head -c 64`
+#ui_print "$? $ret $(pwd) $APKDIR $APPURL"
 
-if [ ! $ret = $SHA256 ];then
-    ui_print ******
-    ui_print sha256 check failed!
-    ui_print Download offical apk not successful, you may need to download
-    ui_print $APPURL by yourself and repack into $ZIPFILE
-    ui_print ******
+if [ "$ret" != "$SHA256" ];then
+    ui_print "******"
+    ui_print "sha256 check failed!"
+    ui_print "Download offical apk not successful, you may need to download"
+    ui_print "$APPURL"
+    ui_print "by yourself and repack into the zip file."
+    ui_print "******"
     abort exit
 fi
