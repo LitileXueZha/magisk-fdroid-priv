adf49d8130a1bba28bd268d484cfc973745aab90
诸葛林 <ktnig@qq.com>
2024-06-21 17:33:43 +0800

    fix Android 4.4

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - https://topjohnwu.github.io/Magisk/guides.html#magisk-modules
 - https://f-droid.org/en/packages/org.fdroid.fdroid.privileged/
 - https://github.com/entr0pia/Fdroid-Priv
+
+## Known Issues
+
+### Xiaomi MIUI/HyperOS Package installer Error -115: null
+
+https://forum.f-droid.org/t/miui-doesnt-allow-privileged-install-error-115-null/
--- customize.sh
+++ customize.sh
@@ -1,12 +1,20 @@
 #!/system/bin/sh
 
 # https://topjohnwu.github.io/Magisk/guides.html#customization
+# https://kernelsu.org/zh_CN/guide/module.html#customizing-installation
 #
 
 
-APK="$MODPATH/system/priv-app/FDroidPrivilegedExtension/base.apk"
+DIR="$MODPATH/system/priv-app/FDroidPrivilegedExtension"
+APK="$DIR/base.apk"
 SHA256="1008525a17b4f6a93ac690f9c50dcb675b6bebf53d2879dbc98ba65a1cb2e28d"
 APPURL="https://f-droid.org/repo/org.fdroid.fdroid.privileged_2130.apk"
+
+# Fix bootloop for devices API < Android 5.0
+if [ $API -lt 21 ];then
+    APK = "$DIR.apk"
+    rm -rf "$DIR"
+fi
 
 if [ ! -f $APK ];then
     ui_print "downloading apk..."
@@ -17,10 +25,11 @@ ret=`sha256sum $APK | head -c 64`
 
 if [ "$ret" != "$SHA256" ];then
     ui_print "******"
-    ui_print "sha256 check failed!"
+    ui_print "SHA256 check failed!"
     ui_print "Download offical apk not successful, you may need to download"
     ui_print "$APPURL"
     ui_print "by yourself and repack into the zip file."
     ui_print "******"
-    abort exit
+    abort "exit"
 fi
+chcon u:object_r:system_file:s0 $APK
